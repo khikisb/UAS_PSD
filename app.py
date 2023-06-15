243d05836ccf8616e84fae951d8e41e56eb4fd6f
khikisb <89850965+khikisb@users.noreply.github.com>
2023-06-15 17:06:29 +0700

    Update app.py

--- app.py
+++ app.py
@@ -6,12 +6,6 @@ import streamlit as st
 model = pickle.load(open('knn_model.pkl', 'rb'))
 model2 = pickle.load(open('naive_bayes_model.pkl', 'rb'))
 
-
-# Define the prediction function
-def predict(Open, High, Low, Close, Volume):
-    prediction = model.predict(pd.DataFrame([[Open, High, Low, Close, Volume]], columns = ['"Open, High, Low, Close, Volume"',]))
-    return prediction
-
 st.set_page_config(
     page_title="Prediksi Nilai Saham Bank BRI",
     page_icon="👋",
@@ -25,26 +19,37 @@ st.title("Prediksi Nilai Saham Bank BRI")
 tab1, tab2, tab3, tab4 = st.tabs(["Data", "Preprocessing Data", "Modeling", "Implementasi"])
 
 with tab1:
-   st.image("TabDD1.png")
+   st.image("data.png")
 
 with tab2:
-   st.image("TabPP1.png")
+   st.write("Reduksi Dimensi")
+   st.image("ReduksiDimensi.png")
+   st.write("MinMax Scaller")
+   st.image("MMScaler.png")
     
 with tab3:
-   st.image("tabmodel.png")
+   st.title("PredBRI")
 
 with tab4:
-   st.image("TI1.png")
-   st.image("TI2.png")
+    # Define the prediction function
+    def predict(Open, High, Low, Close, Volume):
+        prediction = model.predict(pd.DataFrame([[Open, High, Low, Close, Volume]], columns = ['"Open, High, Low, Close, Volume"',]))
+        return prediction
+
+    def predict2(Open, High, Low, Close, Volume):
+        prediction2 = model2.predict2(pd.DataFrame([[Open, High, Low, Close, Volume]], columns = ['"Open, High, Low, Close, Volume"',]))
+        return prediction2
     
-st.header('Jawablah Semua Pertanyaan Berikut :')
+    st.header('Jawablah Semua Pertanyaan Berikut :')
 
-Open = st.selectbox('Apakah anak-anak aman di antara anggota keluarga seperti kakek nenek, paman, bibi, sepupu', ['Setuju', 'Tidak Setuju'])
-High = st.selectbox('Anak-anak paling sering dilecehkan oleh orang asing di masyarakat kita', ['Setuju', 'Tidak Setuju'])
-Low = st.selectbox('Anak laki-laki tidak membutuhkan pengetahuan pencegahan pelecehan seksual', ['Setuju', 'Tidak Setuju'])
-Close = st.selectbox('Mengajarkan pencegahan pelecehan seksual di sekolah tidak perlu. Itu akan membuat anak penasaran dengan seks', ['Setuju', 'Tidak Setuju'])
-Volume = st.selectbox('Apakah anda tahu apa itu perawatan anak?', ['Iya Tahu', 'Tidak Tahu'])
+    Open = st.selectbox('Apakah anak-anak aman di antara anggota keluarga seperti kakek nenek, paman, bibi, sepupu', ['Setuju', 'Tidak Setuju'])
+    High = st.selectbox('Anak-anak paling sering dilecehkan oleh orang asing di masyarakat kita', ['Setuju', 'Tidak Setuju'])
+    Low = st.selectbox('Anak laki-laki tidak membutuhkan pengetahuan pencegahan pelecehan seksual', ['Setuju', 'Tidak Setuju'])
+    Close = st.selectbox('Mengajarkan pencegahan pelecehan seksual di sekolah tidak perlu. Itu akan membuat anak penasaran dengan seks', ['Setuju', 'Tidak Setuju'])
+    Volume = st.selectbox('Apakah anda tahu apa itu perawatan anak?', ['Iya Tahu', 'Tidak Tahu'])
 
-if st.button('Prediksi'):
-    prediksi = predict(Open, High, Low, Close, Volume)
-    st.success(f'Prediksi harga penutupan: {prediksi}')
+    if st.button('Prediksi'):
+        prediksi = predict(Open, High, Low, Close, Volume)
+        prediksi2 = predict2(Open, High, Low, Close, Volume)
+        st.success(f'Prediksi harga penutupan KNN: {prediksi}')
+        st.success(f'Prediksi harga penutupan Menggunakan Naive Bayes: {prediksi2}')
